584bf84def34d2b7661b0d8237a185e740682efe
Nate Rubin <nrubin@wildcats.unh.edu>
2016-12-08 16:31:02 -0500

    day three pt 2

--- day3/day3.scm
+++ day3/day3.scm
@@ -1,19 +1,43 @@
+(define triangle-1 '())
+(define triangle-2 '())
+(define triangle-3 '())
+
 (define (is-triangle-possible? triangle)
   (and
     (> (+ (car triangle) (cadr triangle)) (caddr triangle))
     (> (+ (car triangle) (caddr triangle)) (cadr triangle))
     (> (+ (cadr triangle) (caddr triangle)) (car triangle))))
 
-(define (parse-triangle triangle)
-  (map string->number (string-split triangle)))
+(define (parse-line line)
+  (map string->number (string-split line)))
 
-(define (count-possible-triangles triangle count)
-  (if (not (eof-object? triangle))
-    (if (is-triangle-possible? (parse-triangle triangle))
-        (count-possible-triangles (read-line) (add1 count))
-        (count-possible-triangles (read-line) count))
-    count))
+(define (append-to-triangles line)
+  (let ((parsed-line (parse-line line)))
+   (set! triangle-1 (cons (car parsed-line) triangle-1))
+   (set! triangle-2 (cons (cadr parsed-line) triangle-2))
+   (set! triangle-3 (cons (caddr parsed-line) triangle-3))))
+
+(define (reset-triangles)
+  (set! triangle-1 '())
+  (set! triangle-2 '())
+  (set! triangle-3 '()))
+
+(define (count-possible-triangles line count)
+  (if (eof-object? line)
+    count
+    (begin
+      (append-to-triangles line)
+      (if (eq? 3 (length triangle-1))
+        (let ((possible-triangles 0))
+         (if (is-triangle-possible? triangle-1)
+           (set! possible-triangles (add1 possible-triangles)))
+         (if (is-triangle-possible? triangle-2)
+           (set! possible-triangles (add1 possible-triangles)))
+         (if (is-triangle-possible? triangle-3)
+           (set! possible-triangles (add1 possible-triangles)))
+         (reset-triangles)
+         (count-possible-triangles (read-line) (+ count possible-triangles)))
+        (count-possible-triangles (read-line) count)))))
 
 (define (main args)
   (print (count-possible-triangles (read-line) 0)))
-
