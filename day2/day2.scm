e0a0ddbfcc64f4c55283733fdbd1865472dab817
Nate Rubin <nrubin@wildcats.unh.edu>
2016-12-08 15:05:43 -0500

    day two pt 2

--- day2/day2.scm
+++ day2/day2.scm
@@ -1,22 +1,35 @@
-(define col 1)
-(define row 1)
+(define col 0)
+(define row 2)
 
 (define keypad
-  '((1 2 3)
-    (4 5 6)
-    (7 8 9)))
+  '((-1 -1 1 -1 -1)
+    (-1 2 3 4 -1)
+    (5 6 7 8 9)
+    (-1 \#A \#B \#C -1)
+    (-1 -1 \#D -1 -1)))
 
 (define code '())
 
-(define (move direction)
+(define (move-finger direction)
   (cond
     ((equal? direction #\U) (if (eq? row 0) '() (set! row (sub1 row))))
-    ((equal? direction #\D) (if (eq? row 2) '() (set! row (add1 row))))
+    ((equal? direction #\D) (if (eq? row 4) '() (set! row (add1 row))))
     ((equal? direction #\L) (if (eq? col 0) '() (set! col (sub1 col))))
-    ((equal? direction #\R) (if (eq? col 2) '() (set! col (add1 col))))))
+    ((equal? direction #\R) (if (eq? col 4) '() (set! col (add1 col))))))
+
+(define (key-under-finger)
+  (list-ref (list-ref keypad row) col))
+
+(define (move direction)
+  (let ((old-col col) (old-row row))
+   (move-finger direction)
+   (if (equal? (key-under-finger) -1)
+     (begin
+        (set! col old-col)
+        (set! row old-row)))))
 
 (define (add-number-to-code)
-  (set! code (append code (list (list-ref (list-ref keypad row) col)))))
+  (set! code (append code (list (key-under-finger)))))
 
 (define (follow-line line)
   (map move (string->list line)))
