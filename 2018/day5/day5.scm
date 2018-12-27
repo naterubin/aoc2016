8358a5398b7292c223c78c8682eb50da831f0d58
Nate Rubin <nrubin@appliedgeosolutions.com>
2018-12-27 17:50:12 -0500

    Day 5 part 2

--- 2018/day5/day5.scm
+++ 2018/day5/day5.scm
@@ -1,3 +1,5 @@
+(use-modules (srfi srfi-1))
+
 (define (opposite-polarity unit1 unit2)
   (if (char-lower-case? unit1)
     (char-upper-case? unit2)
@@ -8,15 +10,40 @@
     (eq? (char-upcase unit1) (char-upcase unit2))
     #f))
 
-(define polymer '())
-
-(call-with-input-file "input.txt"
-  (lambda (input)
-    (begin
+(define (react-polymer input)
+    (let ((polymer '()))
       (set! polymer (cons (read-char input) polymer))
       (do ((unit (read-char input) (read-char input)))
           ((eof-object? unit) #nil)
             (if (and (not (= 0 (length polymer))) (units-cancel? unit (car polymer)))
               (set! polymer (cdr polymer))
               (set! polymer (cons unit polymer))))
-      (display (length polymer)))))
+      polymer))
+
+(display "Count after initial reaction: ")
+(display (length (call-with-input-file "input.txt" react-polymer)))
+(newline)
+
+(define (make-exclude-port inport exclude-char)
+  (make-soft-port
+    (vector
+      #f
+      #f
+      #f
+      (lambda ()
+        (let loop ((next-char (read-char inport)))
+         (if (and (not (eof-object? next-char))
+                  (eq? (char-upcase next-char) (char-upcase exclude-char)))
+           (loop (read-char inport))
+           next-char)))
+      (lambda () (close inport)))
+    "r"))
+
+(for-each (lambda (ch)
+            (let ((ex-port (make-exclude-port (open-file "input.txt" "r") ch)))
+                  (display ch)
+                  (display ": ")
+                  (display (length (react-polymer ex-port)))
+                  (newline)
+                  (close ex-port)))
+          (map integer->char (iota 26 97)))
