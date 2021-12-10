d92147d0c79c48870e03f4b3ad2c24bb548062bf
Nate Rubin <contact@naterubin.com>
2021-12-10 15:29:09 -0500

    Start day 2

---
+++ 2021/day2/day2.exs
@@ -0,0 +1,43 @@
+defmodule Submarine do
+  def parse_command(cmd_str) do
+    case String.split(cmd_str, " ") do
+      ["forward", qty] -> {:forward, elem(Integer.parse(qty), 0)}
+      ["down", qty] -> {:down, elem(Integer.parse(qty), 0)}
+      ["up", qty] -> {:up, elem(Integer.parse(qty), 0)}
+    end
+  end
+
+  def execute_commands(commands) do
+    move(commands, {0, 0})
+  end
+
+  def get_result({hrz, vrt}) do
+    hrz * vrt
+  end
+
+  defp move([], acc) do
+    acc
+  end
+
+  defp move(commands, {hrz, vrt}) do
+    [command | rest] = commands
+    new_pos = case command do
+                {:forward, qty} -> {hrz + qty, vrt}
+                {:up, qty} -> {hrz, vrt - qty}
+                {:down, qty} -> {hrz, vrt + qty}
+              end
+    move(rest, new_pos)
+  end
+
+  def get_input(file_name) do
+    {:ok, input} = File.read(file_name)
+    commands = String.split(input, "\n")
+    {commands, _} = Enum.split(commands, length(commands) - 1)
+    commands
+  end
+end
+
+IO.inspect(Submarine.get_input("input.txt")
+    |> Enum.map(&Submarine.parse_command/1)
+    |> Submarine.execute_commands
+    |> Submarine.get_result)
